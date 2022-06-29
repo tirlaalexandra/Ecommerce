1ba5679f188dc571791c06f149cf2f7e4cecfd59
cociubeialexandra <cociubeialexandra@gmail.com>
2022-06-29 23:09:48 +0300

    End of section4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,9 @@
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-
+using AutoMapper;
 namespace API
 {
     public class Startup
@@ -14,12 +15,12 @@ namespace API
     
         }
 
-       
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IProductRepository,ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>),(typeof(GenericRepository<>)));
+            services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
@@ -41,6 +42,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
