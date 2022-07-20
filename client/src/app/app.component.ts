e71fd9b6cc4a962a51eb844c1650bdf20007a053
cociubeialexandra <cociubeialexandra@gmail.com>
2022-07-20 16:37:03 +0300

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +7,6 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Ecomm';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:7106/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-       this.products = response.data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+  ngOnInit(): void {}
 }
