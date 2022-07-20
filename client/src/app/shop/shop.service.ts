fa7110953b1ee692fa2658dd856b567bd32847c6
cociubeialexandra <cociubeialexandra@gmail.com>
2022-07-20 19:36:38 +0300

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -27,7 +28,6 @@ export class ShopService {
 
     if (shopParams.search) {
       params = params.append('search', shopParams.search);
-
     }
 
     params = params.append('sort', shopParams.sort);
@@ -44,6 +44,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
